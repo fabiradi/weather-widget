756004e6319eb6b228ccd6e14647e1d8e626e9e7
Fabian Rademacher <fabianrademacher@gmx.de>
2021-07-28 23:03:33 +0200

    ESLint: Cleanup

--- src/views/Daily.tsx
+++ src/views/Daily.tsx
@@ -2,11 +2,10 @@ import { Fragment } from 'react'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone' // dependent on utc plugin
-import { renderTime, renderDay, renderDayShort } from '../utils/time'
+import { renderTime, renderDayShort } from '../utils/time'
 import styled from 'styled-components'
 import {
   Line,
-  Bar,
   ComposedChart,
   Area,
   XAxis,
@@ -14,7 +13,6 @@ import {
   LabelList,
   Tooltip,
   CartesianGrid,
-  ReferenceLine,
 } from 'recharts'
 
 import { DailyProps } from '../OpenWeatherMapProps'
@@ -22,116 +20,6 @@ import Weather from '../components/Weather'
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
-
-const SingleDay = ({ data }: { data: DailyProps }) => {
-  return (
-    <Table>
-      <tbody>
-        <tr>
-          <th colSpan={2}>{renderDay(data.dt)}</th>
-        </tr>
-        <tr>
-          <th>Rise/Set</th>
-          <td>
-            {renderTime(data.sunrise)} / {renderTime(data.sunset)}
-          </td>
-        </tr>
-        <tr>
-          <th>T.Min/Max</th>
-          <td>
-            {data.temp.min}°C / {data.temp.max}°C
-          </td>
-        </tr>
-        <tr>
-          <th>T.Morn</th>
-          <td>
-            {data.temp.morn}°C ({data.feels_like.morn}°C)
-          </td>
-        </tr>
-        <tr>
-          <th>T.Day</th>
-          <td>
-            {data.temp.day}°C ({data.feels_like.day}°C)
-          </td>
-        </tr>
-        <tr>
-          <th>T.Eve</th>
-          <td>
-            {data.temp.eve}°C ({data.feels_like.eve}°C)
-          </td>
-        </tr>
-        <tr>
-          <th>T.Night</th>
-          <td>
-            {data.temp.night}°C ({data.feels_like.night}°C)
-          </td>
-        </tr>
-        <tr>
-          <th>Press</th>
-          <td>{data.pressure}hPa</td>
-        </tr>
-        <tr>
-          <th>Hum.</th>
-          <td>{data.humidity}%</td>
-        </tr>
-        <tr>
-          <th>Dew</th>
-          <td>{data.dew_point}°C</td>
-        </tr>
-        <tr>
-          <th>Wind Speed</th>
-          <td>{data.wind_speed} m/s</td>
-        </tr>
-        {data.wind_gust && (
-          <tr>
-            <th>Wind Gust</th>
-            <td>{data.wind_gust} m/s</td>
-          </tr>
-        )}
-        <tr>
-          <th>Wind Deg</th>
-          <td>{data.wind_deg}°</td>
-        </tr>
-        <tr>
-          <th>Clouds</th>
-          <td>{data.clouds}%</td>
-        </tr>
-        <tr>
-          <th>UVI</th>
-          <td>{data.uvi}</td>
-        </tr>
-        <tr>
-          <th>Pop</th>
-          <td>{(data.pop * 100).toFixed(0)}%</td>
-        </tr>
-        <tr>
-          <th>Rain</th>
-          <td>{data.rain ? `${data.rain} mm` : ''}</td>
-        </tr>
-        <tr>
-          <th>Snow</th>
-          <td>{data.snow ? `${data.snow} mm` : ''}</td>
-        </tr>
-        <tr>
-          <th>Weather</th>
-          {data.weather.map((item, i) => (
-            <td colSpan={2} key={i}>
-              <Weather.Small data={item} />
-            </td>
-          ))}
-        </tr>
-        <tr>
-          <th>Weather</th>
-          {data.weather.map((item, i) => (
-            <td key={i}>
-              <Weather.Large data={item} />
-            </td>
-          ))}
-        </tr>
-      </tbody>
-    </Table>
-  )
-}
 
 const CombinedDays = ({ data }: { data: DailyProps[] }) => {
   return (
